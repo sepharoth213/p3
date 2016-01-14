d437edb0204160f66451dba6bc157dda37862c27
Aaron Santiago <aaronsantiago@gmail.com>
2016-01-13 19:31:53 -0500

    Add addresses to addr.py

--- p3/addr.py
+++ p3/addr.py
@@ -9,7 +9,7 @@ Defines some addresses, and generates a names dictionary
 
 '''
 
-def MultipleAddress(addressType,args,totalAddresses,addressOffset,nameOffset = 1):
+def MultipleAddress(addressType,args,addressOffset,totalAddresses = 4,nameOffset = 1):
     addressObjects = []
     namePreSplit, namePostSplit = args[0].split('#')
     baseAddress = [i for i in map(lambda o: int(o, 16), args[1].split())]
@@ -23,61 +23,40 @@ def MultipleAddress(addressType,args,totalAddresses,addressOffset,nameOffset = 1
                 address += hex(b + o*i)[2:] + " "
             else:
                 address += hex(b)[2:] + " "
-        address = address[:-1]
+        address = str.upper(address[:-1])
 
         newArgs = tuple([name,address] + list(args[2:]))
-        print(newArgs)
         addressObjects.append(addressType(*newArgs))
     return addressObjects
 
+
 names = {}
 
-
 addresses = [
-    at.IntegerAddress("playerOnePercent", "804530E0", 16),
-    at.IntegerAddress("playerTwoPercent", "80453F70", 16),
-
-    at.IntegerAddress("playerOneStocks", "8045310E", 24),
-    at.IntegerAddress("playerTwoStocks", "80453F9E", 24),
-    
-    at.IntegerAddress("playerOnePercent", "804530E0", 16),
-    at.IntegerAddress("playerTwoPercent", "80453F70", 16),
-    
-    at.IntegerAddress("playerOneChar", "803F0E08", 0, 0xFF),
-    at.IntegerAddress("playerTwoChar", "803F0E2C", 0, 0xFF),
-    
-    at.FloatAddress("playerOneX", "80453090"),
-    at.FloatAddress("playerOneY", "80453094"),
-    
-    at.FloatAddress("playerTwoX", "80453F20"),
-    at.FloatAddress("playerTwoY", "80453F24"),
     
     at.IntegerAddress("stageID", "804D6CAD", 16),
     at.IntegerAddress("frameCount", "8046B6CC", 16),
     at.IntegerAddress("currentMenu", "8065CC14", 20, 0xF),
 
-    # at.IntegerAddress("playerOneActionState", "80453130 70"),
-    # at.FloatAddress("playerOneDeltaY", "80453130 12C"),
-    # at.IntegerAddress("playerOneInAir", "80453130 140"),
-    # at.FloatAddress("playerOneHitlagFrames", "80453130 19BC"),
-    # at.IntegerAddress("playerOneJumpsUsed", "80453130 19C8", 24),
-    # at.FloatAddress("playerOneShieldSize", "80453130 19F8"),
-    # at.FloatAddress("playerOneHitstunFrames", "80453130 23A0"),
-
-    # at.IntegerAddress("playerTwoActionState", "80453FC0 70"),
-    # at.FloatAddress("playerTwoDeltaY", "80453FC0 12C"),
-    # at.IntegerAddress("playerTwoInAir", "80453FC0 140"),
-    # at.FloatAddress("playerTwoHitlagFrames", "80453FC0 19BC"),
-    # at.IntegerAddress("playerTwoJumpsUsed", "80453FC0 19C8",24),
-    # at.FloatAddress("playerTwoShieldSize", "80453FC0 19F8"),
-    # at.FloatAddress("playerTwoHitstunFrames", "80453FC0 23A0"),
-
-    at.IntegerAddress("globalFrameCounter", "804D7420"),
+    # at.IntegerAddress("globalFrameCounter", "804D7420"),
 ]
 
-addresses += MultipleAddress(at.IntegerAddress,("player#Percent", "804530E0", 16) , 4, "E90")
+addresses += MultipleAddress( at.IntegerAddress, ("player#Percent",     "804530E0", 16),        "E90")
+addresses += MultipleAddress( at.IntegerAddress, ("player#Stocks",      "8045310E", 24),        "E90")
+addresses += MultipleAddress( at.IntegerAddress, ("player#Character",   "803F0E08", 0, 0xFF) ,  "14" )
+addresses += MultipleAddress( at.IntegerAddress, ("player#ActionState", "80453130 70"),         "E90")
+addresses += MultipleAddress( at.IntegerAddress, ("player#JumpsUsed",   "80453130 19C8",24),    "E90")
+
+addresses += MultipleAddress( at.BooleanAddress, ("player#InAir", "80453130 140", "1"), "E90")
+
+addresses += MultipleAddress( at.FloatAddress,   ("player#X",             "80453090"),      "E90")
+addresses += MultipleAddress( at.FloatAddress,   ("player#Y",             "80453094"),      "E90")
+addresses += MultipleAddress( at.FloatAddress,   ("player#DeltaY",        "80453130 12C"),  "E90")
+addresses += MultipleAddress( at.FloatAddress,   ("player#HitlagFrames",  "80453130 19BC"), "E90")
+addresses += MultipleAddress( at.FloatAddress,   ("player#ShieldSize",    "80453130 19F8"), "E90")
+addresses += MultipleAddress( at.FloatAddress,   ("player#HitstunFrames", "80453130 23A0"), "E90")
 
 locationsTxt = ""
 for address in addresses:
-    locationsTxt += address.name + "\n"
+    locationsTxt += address.address + "\n"
     names[address.address] = address
--- p3/at.py
+++ p3/at.py
@@ -1,6 +1,7 @@
+import struct
 
 class IntegerAddress:
-    def __init__(self,name,address,shift, mask = 0xFFFFFFFF):
+    def __init__(self,name,address,shift = 0, mask = 0xFFFFFFFF):
         self.name = name
         self.address = address
         self.mask = mask
@@ -13,9 +14,17 @@ class FloatAddress:
     def __init__(self,name,address):
         self.name = name
         self.address = address
-        self.mask = mask
-        self.shift = shift
 
     def get_value(self,value):
-        # return (int.from_bytes(value,byteorder='big') >> self.shift) & self.mask 
-        return 0
+        return struct.unpack('f', value)[0]
+
+class BooleanAddress:
+    def __init__(self,name,address,compareValue,shift = 0, mask = 0xFFFFFFFF):
+        self.name = name
+        self.address = address
+        self.mask = mask
+        self.shift = shift
+        self.compareValue = compareValue
+
+    def get_value(self,value):
+        return ((int.from_bytes(value,byteorder='big') >> self.shift) & self.mask == int(self.compareValue, 16))
