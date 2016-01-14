5349f33513e52256b0d11779b87d1f14034871c7
Aaron Santiago <aaronsantiago@gmail.com>
2016-01-14 07:27:29 -0500

    Wrap address fields in an Address class

--- p3/addr.py
+++ p3/addr.py
@@ -5,7 +5,7 @@ from p3 import at
 
 '''
 
-Defines some addresses, and generates a names dictionary
+Defines some addresses, and generates a byAddress dictionary
 
 '''
 
@@ -29,34 +29,34 @@ def MultipleAddress(addressType,args,addressOffset,totalAddresses = 4,nameOffset
         addressObjects.append(addressType(*newArgs))
     return addressObjects
 
+class Address:
+    byAddress = {}
 
-names = {}
+    addresses = [
+        
+        at.IntegerAddress("stageID", "804D6CAD", 16),
+        at.IntegerAddress("frameCount", "8046B6CC", 16),
+        at.IntegerAddress("currentMenu", "8065CC14", 20, 0xF),
 
-addresses = [
-    
-    at.IntegerAddress("stageID", "804D6CAD", 16),
-    at.IntegerAddress("frameCount", "8046B6CC", 16),
-    at.IntegerAddress("currentMenu", "8065CC14", 20, 0xF),
+        # at.IntegerAddress("globalFrameCounter", "804D7420"),
+    ]
 
-    # at.IntegerAddress("globalFrameCounter", "804D7420"),
-]
+    addresses += MultipleAddress( at.IntegerAddress, ("player#Percent",     "804530E0", 16),        "E90")
+    addresses += MultipleAddress( at.IntegerAddress, ("player#Stocks",      "8045310E", 24),        "E90")
+    addresses += MultipleAddress( at.IntegerAddress, ("player#Character",   "803F0E08", 0, 0xFF) ,  "14" )
+    addresses += MultipleAddress( at.IntegerAddress, ("player#ActionState", "80453130 70"),         "E90")
+    addresses += MultipleAddress( at.IntegerAddress, ("player#JumpsUsed",   "80453130 19C8",24),    "E90")
 
-addresses += MultipleAddress( at.IntegerAddress, ("player#Percent",     "804530E0", 16),        "E90")
-addresses += MultipleAddress( at.IntegerAddress, ("player#Stocks",      "8045310E", 24),        "E90")
-addresses += MultipleAddress( at.IntegerAddress, ("player#Character",   "803F0E08", 0, 0xFF) ,  "14" )
-addresses += MultipleAddress( at.IntegerAddress, ("player#ActionState", "80453130 70"),         "E90")
-addresses += MultipleAddress( at.IntegerAddress, ("player#JumpsUsed",   "80453130 19C8",24),    "E90")
+    addresses += MultipleAddress( at.BooleanAddress, ("player#InAir", "80453130 140", "1"), "E90")
 
-addresses += MultipleAddress( at.BooleanAddress, ("player#InAir", "80453130 140", "1"), "E90")
+    addresses += MultipleAddress( at.FloatAddress,   ("player#X",             "80453090"),      "E90")
+    addresses += MultipleAddress( at.FloatAddress,   ("player#Y",             "80453094"),      "E90")
+    addresses += MultipleAddress( at.FloatAddress,   ("player#DeltaY",        "80453130 12C"),  "E90")
+    addresses += MultipleAddress( at.FloatAddress,   ("player#HitlagFrames",  "80453130 19BC"), "E90")
+    addresses += MultipleAddress( at.FloatAddress,   ("player#ShieldSize",    "80453130 19F8"), "E90")
+    addresses += MultipleAddress( at.FloatAddress,   ("player#HitstunFrames", "80453130 23A0"), "E90")
 
-addresses += MultipleAddress( at.FloatAddress,   ("player#X",             "80453090"),      "E90")
-addresses += MultipleAddress( at.FloatAddress,   ("player#Y",             "80453094"),      "E90")
-addresses += MultipleAddress( at.FloatAddress,   ("player#DeltaY",        "80453130 12C"),  "E90")
-addresses += MultipleAddress( at.FloatAddress,   ("player#HitlagFrames",  "80453130 19BC"), "E90")
-addresses += MultipleAddress( at.FloatAddress,   ("player#ShieldSize",    "80453130 19F8"), "E90")
-addresses += MultipleAddress( at.FloatAddress,   ("player#HitstunFrames", "80453130 23A0"), "E90")
-
-locationsTxt = ""
-for address in addresses:
-    locationsTxt += address.address + "\n"
-    names[address.address] = address
+    locationsTxt = ""
+    for address in addresses:
+        locationsTxt += address.address + "\n"
+        byAddress[address.address] = address
