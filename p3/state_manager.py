46edaaaabdbc3056434433cb6dda4ead43784d69
Aaron Santiago <aaronsantiago@gmail.com>
2016-02-04 02:07:35 -0500

    Change callbacks to pass the full state rather than the local object

--- p3/state_manager.py
+++ p3/state_manager.py
@@ -7,42 +7,6 @@ from p3.state import Menu
 from p3.state import Stage
 from p3.state import ActionState
 
-def int_handler(obj, name, shift=0, mask=0xFFFFFFFF, wrapper=None, default=0):
-    """Returns a handler that sets an attribute for a given object.
-
-    obj is the object that will have its attribute set. Probably a State.
-    name is the attribute name to be set.
-    shift will be applied before mask.
-    Finally, wrapper will be called on the value if it is not None.
-
-    This sets the attribute to default when called. Note that the actual final
-    value doesn't need to be an int. The wrapper can convert int to whatever.
-    This is particularly useful for enums.
-    """
-    def handle(value):
-        transformed = (struct.unpack('>i', value)[0] >> shift) & mask
-        wrapped = transformed if wrapper is None else wrapper(transformed)
-        setattr(obj, name, wrapped)
-        for cb in getattr(obj, name + "_changed"):
-            cb(obj)
-    setattr(obj, name, default)
-    setattr(obj, name + "_changed", [])
-    return handle
-
-def float_handler(obj, name, wrapper=None, default=0.0):
-    """Returns a handler that sets an attribute for a given object.
-
-    Similar to int_handler, but no mask or shift.
-    """
-    def handle(value):
-        as_float = struct.unpack('>f', value)[0]
-        setattr(obj, name, as_float if wrapper is None else wrapper(as_float))
-        for cb in getattr(obj, name + "_changed"):
-            cb(obj)
-    setattr(obj, name, default)
-    setattr(obj, name + "_changed", [])
-    return handle
-
 def add_address(x, y):
     """Returns a string representation of the sum of the two parameters.
 
@@ -58,9 +22,9 @@ class StateManager:
         self.state = state
         self.addresses = {}
 
-        self.addresses['80479D60'] = int_handler(self.state, 'frame')
-        self.addresses['80479D30'] = int_handler(self.state, 'menu', 0, 0xFF, Menu, Menu.Characters)
-        self.addresses['804D6CAC'] = int_handler(self.state, 'stage', 8, 0xFF, Stage, Stage.Unselected)
+        self.addresses['80479D60'] = self.int_handler(self.state, 'frame')
+        self.addresses['80479D30'] = self.int_handler(self.state, 'menu', 0, 0xFF, Menu, Menu.Characters)
+        self.addresses['804D6CAC'] = self.int_handler(self.state, 'stage', 8, 0xFF, Stage, Stage.Unselected)
 
         self.state.players = []
         for player_id in range(4):
@@ -69,16 +33,16 @@ class StateManager:
             data_pointer = add_address('80453130', 0xE90 * player_id)
 
             type_address = add_address('803F0E08', 0x24 * player_id)
-            type_handler = int_handler(player, 'type', 24, 0xFF, PlayerType, PlayerType.Unselected)
-            character_handler = int_handler(player, 'character', 8, 0xFF, Character, Character.Unselected)
+            type_handler = self.int_handler(player, 'type', 24, 0xFF, PlayerType, PlayerType.Unselected)
+            character_handler = self.int_handler(player, 'character', 8, 0xFF, Character, Character.Unselected)
             self.addresses[type_address] = [type_handler, character_handler]
 
             state_address = data_pointer + ' 70'
-            state_handler = int_handler(player, 'action_state', 0, 0xFFFF, ActionState, ActionState.Unselected)
+            state_handler = self.int_handler(player, 'action_state', 0, 0xFFFF, ActionState, ActionState.Unselected)
             self.addresses[state_address] = state_handler
 
             ground_address = data_pointer + ' 140'
-            ground_handler = int_handler(player, 'on_ground', 0, 0xFFFF, lambda x: x == 0, True)
+            ground_handler = self.int_handler(player, 'on_ground', 0, 0xFFFF, lambda x: x == 0, True)
             self.addresses[ground_address] = ground_handler
 
     def handle(self, address, value):
@@ -94,3 +58,40 @@ class StateManager:
     def locations(self):
         """Returns a list of addresses for exporting to Locations.txt."""
         return self.addresses.keys()
+
+
+    def int_handler(self, obj, name, shift=0, mask=0xFFFFFFFF, wrapper=None, default=0):
+        """Returns a handler that sets an attribute for a given object.
+
+        obj is the object that will have its attribute set. Probably a State.
+        name is the attribute name to be set.
+        shift will be applied before mask.
+        Finally, wrapper will be called on the value if it is not None.
+
+        This sets the attribute to default when called. Note that the actual final
+        value doesn't need to be an int. The wrapper can convert int to whatever.
+        This is particularly useful for enums.
+        """
+        def handle(value):
+            transformed = (struct.unpack('>i', value)[0] >> shift) & mask
+            wrapped = transformed if wrapper is None else wrapper(transformed)
+            setattr(obj, name, wrapped)
+            for cb in getattr(obj, name + "_changed"):
+                cb(self.state)
+        setattr(obj, name, default)
+        setattr(obj, name + "_changed", [])
+        return handle
+
+    def float_handler(self, obj, name, wrapper=None, default=0.0):
+        """Returns a handler that sets an attribute for a given object.
+
+        Similar to int_handler, but no mask or shift.
+        """
+        def handle(value):
+            as_float = struct.unpack('>f', value)[0]
+            setattr(obj, name, as_float if wrapper is None else wrapper(as_float))
+            for cb in getattr(obj, name + "_changed"):
+                cb(self.state)
+        setattr(obj, name, default)
+        setattr(obj, name + "_changed", [])
+        return handle
